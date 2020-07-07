587da407106f811a491c9193285470a97ce04de9
Arnab Jana <arnab.jana@walmartlabs.com>
2020-07-07 16:38:51 +0530

    Leetcode 2nd problem added

--- edit.cpp
+++ edit.cpp
@@ -7,55 +7,64 @@ typedef long long int llt;
 
 using namespace std;
 
-vector<vector<int> > g;
-vector<bool> vis;
-
-void ipgraph(int m) {
-    int x,y;
-    for(int i=0;i<m;i++) {
-        cin>>x>>y;
-        g[x].push_back(y);
-        g[y].push_back(x);
-    }
-}
-
-void dfs(int s) {
-    vis[s]=true;
-    cout<<s<<"->";
-    for(int i=0;i<g[s].size();i++) {
-        if(vis[g[s][i]]==false) {
-            dfs(g[s][i]);
-        }
-    }
-}
-
-void bfs(int s) {
-    queue<int> q;
-    q.push(s);
-    vis[s]=true;
-    while(!q.empty()) {
-        int front = q.front();
-        q.pop();
-        cout<<front<<"->";
-
-        for(int i=0;i<g[front].size();i++) {
-            if(vis[g[front][i]] == false) {
-                q.push(g[front][i]);
-                vis[g[front][i]]=true;
-            }
-        }
-    }
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    map<int,pair<int,int> > hm;
-    hm[3]=make_pair(1,1);
-    cout<<hm[3].first<<" "<<hm[3].second;
-    cout<<hm[1].first<<" "<<hm[1].second;
-
+    int t,n;
+    cin>>t;
+    while(t--) {
+        cin>>n;
+        llt a[n],b[n];
+        llt res=0;
+        map<llt,llt> hm1,hm2,hmSum,hmOrig;
+        for(int i=0;i<n;i++) {
+            cin>>a[i];
+            hm1[a[i]]++;
+            hmSum[a[i]]++;
+        }
+        for(int i=0;i<n;i++) {
+            cin>>b[i];
+            hm2[b[i]]++;
+            hmSum[b[i]]++;
+        }
+        
+        for(auto item:hmSum) {
+            if(item.second%2 == 1) {
+                res=-1;
+                break;
+            }
+            hmOrig[item.first]=item.second/2;
+        }
+        if(res==-1) {
+            cout<<-1<<endl;
+            continue;
+        }
+        vector<pair<llt,llt> > v;
+        for(auto item:hm1) {
+            if(item.second>hmOrig[item.first]) {
+                v.push_back({item.first,item.second-hmOrig[item.first]});
+            }
+        }
+        for(auto item:hm2) {
+            if(item.second>hmOrig[item.first]) {
+                v.push_back({item.first,item.second-hmOrig[item.first]});
+            }
+        }
+        sort(v.begin(),v.end());
+        vector<llt> v1;
+        for(int i=0;i<v.size();i++) {
+            for(int j=0;j<v[i].second;j++) {
+                v1.push_back(v[i].first);
+            }
+        }
+        res=0;
+        for(int i=0;i<v1.size()/2;i++) {
+            res+=v1[i];
+        }
+        cout<<res<<endl;
+    }
+    return 0;
 }
 
 
